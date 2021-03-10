86328d8c2bc34ac66dd734c95dc553e56dbe2aa7
AlonBru <indyalon@gmail.com>
2021-03-10 13:37:12 +0200

    server messasging

--- server/index.js
+++ server/index.js
@@ -1,7 +1,36 @@
-const firebase = require('firebase/app')
-require('firebase/messaging')
-const app = require('./app');
 
+const app = require('./app');
+require('dotenv').config()
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+      projectId: process.env.PROJECT_ID,
+      privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.CLIENT_EMAIL,
+  }),
+});
+
+var registrationToken = 'registration from client';
+
+var message = {
+  notification: {
+    title: 'hi',
+    body: 'wassap',
+  },
+  token: registrationToken
+};
+
+// Send a message to the device corresponding to the provided
+// registration token.
+admin.messaging().send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
 
 const port = process.env.PORT || 3001;
 app.listen(port, function () {
